161b0620f349b902265c39ff958beb8b4a1920a7
Tamas Zadori <tamas.zadori@cpacsystems.se>
2022-12-21 16:40:12 +0100

    Day14 part 2

--- 2022/14.py
+++ 2022/14.py
@@ -7,7 +7,7 @@ class Day14(Puzzle):
     def __init__(self) -> None:
         super().__init__(2022, 14)
     
-    def draw_cave(self, cave_bottom = False):
+    def draw_cave(self):
         rocks = []
         dimensions = [0,0]
         for line in self._data.splitlines():
@@ -26,35 +26,48 @@ class Day14(Puzzle):
                             rocks.append([x1[0], y])
                 x1 = x2
 
-        if cave_bottom:
-            for col in range(dimensions[0]+1):
-                rocks.append([col, dimensions[1]+2])
-            
-            dimensions[1] += 2
-            dimensions[0] += 1
-
         cave = np.matrix(np.zeros((dimensions[1]+1, dimensions[0]+1), dtype=int))
         for rock in rocks:
             cave[rock[1], rock[0]] = 1
         print(cave[:, dimensions[0] - 30:])
         return cave
 
-    def pour_sand(self, cave: np.matrix, cave_bottom = False):
+    def pour_sand(self, cave: np.matrix, cave_bottom: tuple = None):
         sand_source = (0, 500)
         sand_pos = self.find_next_pos(sand_source, cave, cave_bottom)
         particles = 0
-        while sand_pos:
+        while sand_pos and not sand_pos == sand_source:
             particles += 1
+
+            if sand_pos[1] < 0: #expand left
+                col = np.zeros((cave.shape[0], 1), dtype=int)
+                cave = np.hstack((col,cave))
+            if sand_pos[1] == cave.shape[1]:
+                col = np.zeros((cave.shape[0], 1), dtype=int)
+                cave = np.hstack((cave,col))
+            
+            if sand_pos[0] == cave.shape[0]: #expand cave downwards if needed
+                row = np.zeros((1, cave.shape[1]), dtype=int) if sand_pos[0] < cave_bottom else np.ones((1, cave.shape[1]), dtype=int)
+                cave = np.vstack((cave,row))
+
             cave[sand_pos[0], sand_pos[1]] = 2
-            print(cave[:, cave.shape[1] - 30:])
+            #print(cave[:, cave.shape[1] - 30:])
+            print(sand_pos)
             print('-------'*6)
             sand_pos = self.find_next_pos(sand_source, cave, cave_bottom)
         
-        return particles
+        return particles + 1 if sand_pos == sand_source else particles
     
-    def item_at_pos(self, pos, cave):
-        if pos[0]==cave.shape[0]: return None #abyss
-        if pos[1]<0 or pos[1] == cave.shape[1]: return None #abyss
+    def item_at_pos(self, pos, cave, cave_bottom):
+        if pos[0] >=cave.shape[0]:
+            if not cave_bottom:
+                return None
+            
+            return 0 if cave_bottom > pos[0] else 1 #cave bottom!
+        
+        if pos[1] < 0 or pos[1] == cave.shape[1]:
+            return 0 if cave_bottom else None #abyss
+
         return cave[pos[0], pos[1]]
 
     def item_at_pos_with_bottom(self, pos, cave):
@@ -62,7 +75,7 @@ class Day14(Puzzle):
         if pos[1]<0 or pos[1] == cave.shape[1]: return None #abyss
         return cave[pos[0], pos[1]]
 
-    def find_next_pos(self, current_pos: tuple, cave: np.matrix, cave_bottom = False) -> tuple:
+    def find_next_pos(self, current_pos: tuple, cave: np.matrix, cave_bottom: tuple = None) -> tuple:
         
         pos_down = lambda t: (t[0]+1, t[1])
         pos_diag_left = lambda t: (t[0]+1,t[1]-1)
@@ -73,8 +86,8 @@ class Day14(Puzzle):
 
         while not possible_moves.empty():
             next_move = possible_moves.get()
-            item = self.item_at_pos(next_move(current_pos), cave)
-            if item is None:
+            item = self.item_at_pos(next_move(current_pos), cave, cave_bottom)
+            if item is None and not cave_bottom:
                 return None
             elif item == 0: #position free
                 current_pos = next_move(current_pos)
@@ -94,12 +107,12 @@ class Day14(Puzzle):
         return self.pour_sand(cave) # queues with key at the bottom of the drop
 
     def solve_b(self):
-        cave = self.draw_cave(True)
-        return self.pour_sand(cave, True) # queues with key at the bottom of the drop
+        cave = self.draw_cave()
+        return self.pour_sand(cave, cave.shape[0] + 1)
         
 
 d = Day14()
-d.init_data()
-bres = d.solve_b()
-#d.submit(sub_b=False)
-print(bres)
+#d.init_data()
+#bres = d.solve_b()
+d.submit()
+#print(bres)
